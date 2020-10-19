b1865df276e2cfefc6324450092b3a1bf8165bdd
akinator001 <aakashgandhi1279@gmail.com>
2020-10-19 23:00:45 +0530

    [aakash] welcome message

---
+++ main/java/com/cp/indianstatecensusanalyser/StateCensusAnalyser.java
@@ -0,0 +1,8 @@
+package com.cp.indianstatecensusanalyser;
+
+public class StateCensusAnalyser {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to indian state census analyser");
+	}
+}
