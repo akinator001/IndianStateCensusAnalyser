b3cf389cc1e34b27cc0057fa8898138dfc25ca47
akinator001 <aakashgandhi1279@gmail.com>
2020-10-20 00:03:41 +0530

    [aakash] Refactored : cutom exception for wrong file path

--- test/java/com/cp/indianstatecensusanalyser/IndianCensusAnalyserTest.java
+++ test/java/com/cp/indianstatecensusanalyser/IndianCensusAnalyserTest.java
@@ -1,15 +1,31 @@
 package com.cp.indianstatecensusanalyser;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class IndianCensusAnalyserTest {
+	private static final String CSV_FILE = "./Indian.csv";
 	private static String CSV_CENSUS_FILE  = "./IndianStateCensusData.csv";
 
 	@Test
-	public void givenNumberOfEntriesInACSVFile_ShouldReturnExactlytheSameWhileReading() {
+	public void givenNumberOfEntriesInACSVFile_ShouldReturnExactlytheSameWhileReading() throws StateCensusAnalyserException {
 		StateCensusAnalyser obj = new StateCensusAnalyser();
-		int entries = obj.readData();
+		int entries = obj.readData(CSV_CENSUS_FILE);
 		Assert.assertEquals(29, entries);
 	}
+
+	@Test
+	public void givenWrongFileLocationsthrowsCustomeException_ForInvalidFilePath() {
+		StateCensusAnalyser obj = new StateCensusAnalyser();
+		try {
+			obj.readData(CSV_FILE);
+		} catch (StateCensusAnalyserException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+			Assert.assertEquals(StateCensusAnalyserException.ExceptionType.INVALID_FILE_PATH, e.type);
+		}
+	}
+	
 }
