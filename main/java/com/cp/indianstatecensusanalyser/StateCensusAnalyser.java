019c5e06c4d2545b15dec9fe89c8a3aa17414bde
akinator001 <aakashgandhi1279@gmail.com>
2020-10-19 23:22:49 +0530

    [aakash] Added : read indian census data fro csv file

--- main/java/com/cp/indianstatecensusanalyser/StateCensusAnalyser.java
+++ main/java/com/cp/indianstatecensusanalyser/StateCensusAnalyser.java
@@ -1,8 +1,38 @@
 package com.cp.indianstatecensusanalyser;
 
-public class StateCensusAnalyser {
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class StateCensusAnalyser {
+	private static String CSV_CENSUS_FILE = "./IndianStateCensusData.csv";
+
+	@SuppressWarnings("unchecked")
+	public void readData() {
+		int noOfEntries = 0;
+		try {
+			Reader readFile = Files.newBufferedReader(Paths.get(CSV_CENSUS_FILE));
+			CsvToBean<IndianStateCensus> user = new CsvToBeanBuilder(readFile).withType(IndianStateCensus.class)
+												.withIgnoreLeadingWhiteSpace(true).build();
+			Iterator<IndianStateCensus> userIterator = user.iterator();
+			while (userIterator.hasNext()) {
+				IndianStateCensus csvuser = userIterator.next();
+				System.out.println(csvuser);
+				noOfEntries++;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}	
 	public static void main(String[] args) {
 		System.out.println("Welcome to indian state census analyser");
+		StateCensusAnalyser object = new StateCensusAnalyser();
+		object.readData();
 	}
 }
