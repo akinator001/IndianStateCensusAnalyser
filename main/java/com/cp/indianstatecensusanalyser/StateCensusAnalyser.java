dc252be261ed87cf2b069741dc0ec03e2585792d
akinator001 <aakashgandhi1279@gmail.com>
2020-10-19 23:50:20 +0530

    [aakash] Refactored : cutom exception for wrong file path

--- main/java/com/cp/indianstatecensusanalyser/StateCensusAnalyser.java
+++ main/java/com/cp/indianstatecensusanalyser/StateCensusAnalyser.java
@@ -13,10 +13,10 @@ public class StateCensusAnalyser {
 	private static String CSV_CENSUS_FILE = "./IndianStateCensusData.csv";
 
 	@SuppressWarnings("unchecked")
-	public int readData() {
+	public int readData(String DATA_FILE) throws StateCensusAnalyserException {
 		int noOfEntries = 0;
 		try {
-			Reader readFile = Files.newBufferedReader(Paths.get(CSV_CENSUS_FILE));
+			Reader readFile = Files.newBufferedReader(Paths.get(DATA_FILE));
 			CsvToBean<IndianStateCensus> user = new CsvToBeanBuilder(readFile).withType(IndianStateCensus.class)
 												.withIgnoreLeadingWhiteSpace(true).build();
 			Iterator<IndianStateCensus> userIterator = user.iterator();
@@ -26,15 +26,14 @@ public class StateCensusAnalyser {
 				noOfEntries++;
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.INVALID_FILE_PATH , "invalid file location !!");
 		}
 		return noOfEntries;
 				
 	}	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws StateCensusAnalyserException {
 		System.out.println("Welcome to indian state census analyser");
 		StateCensusAnalyser object = new StateCensusAnalyser();
-		object.readData();
+		object.readData(CSV_CENSUS_FILE);
 	}
 }
